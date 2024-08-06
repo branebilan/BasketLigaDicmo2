ff5ccba9d990328ee643efec6fb2be4cc6916f2f
Branimir Bilan <148582574+branebilan@users.noreply.github.com>
2024-08-07 00:56:37 +0200

    Update BasketJS3.js

--- BasketJS3.js
+++ BasketJS3.js
@@ -1,7 +1,11 @@
 import { player1 } from "./BasketJS2";
 
+function playerStats() {
 let playerOne = [player1.points_average, player1.assists_average, player1.rebounds_average];
 for (let i = 0; i < array.length; i++){
   const element = document.querySelector('.element-01' + i);
   element.append(playerOne[i].property);
+ }
 }
+
+playerStats();
